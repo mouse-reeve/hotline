22853b49301f34230efd7cfbcf85e0625d6bd70e
Mouse Reeve <mousereeve@riseup.net>
2017-02-28 13:25:38 -0800

    Minor code cleanup

--- test-app.py
+++ test-app.py
@@ -9,12 +9,14 @@ script = Hotline(json.load(file('script.json')))
 @app.route('/', methods=['GET'])
 @app.route('/<endpoint>', methods=['GET'])
 def call_endpoint(endpoint='start'):
+    ''' receive a call or play a scene '''
     return script.run(endpoint)
 
 
 @app.route('/', methods=['POST'])
 @app.route('/<endpoint>', methods=['POST'])
 def receive_data(endpoint='start'):
+    ''' process a keypress during a phone call '''
     digits = request.form['Digits']
     return script.parse_keypress(endpoint, digits)
 
